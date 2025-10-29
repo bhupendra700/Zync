3166c771366321e104afbbecdef91f8f673f5420
bhupendra700 <bhupendra7021@gmail.com>
2025-10-30 02:39:11 +0530

    uploading more

--- frontend/src/Component/Header.jsx
+++ frontend/src/Component/Header.jsx
@@ -3,9 +3,23 @@ import logo from '../assets/logo.png'
 import my from '../assets/My.jpg'
 import { MdOutlineQrCodeScanner } from "react-icons/md";
 import { CiMenuKebab } from "react-icons/ci";
+import { ImProfile } from "react-icons/im";
+import { GrInfo } from "react-icons/gr";
+import { useEffect, useState } from 'react';
 
 
 const Header = () => {
+
+    const [theme, setTheme] = useState(localStorage.getItem("theme") === "dark" ? true : false);
+
+    useEffect(() => {
+        document.body.classList.toggle("dark");
+    }, [theme]);
+
+    useEffect(() => {
+        document.body.setAttribute("class" , localStorage.getItem("theme") === "dark" ? "dark" : "");
+    }, []);
+
     return <header>
         <section>
             <div className="left">
@@ -16,16 +30,26 @@ const Header = () => {
             </div>
             <div className='right'>
                 <div><MdOutlineQrCodeScanner /></div>
-                <div><img src={my} alt="profile pic" height={100} /></div>
                 <details>
                     <summary>
                         <CiMenuKebab />
                     </summary>
                     <div>
-                        <div>Edit profile</div>
+                        <div><img src={my} alt="profile pic" height={100} /></div>
+                        <div>
+                            <button><ImProfile /> Edit Profile</button>
+                            <button><GrInfo /> About</button>
+                            <button>
+                                <label htmlFor={"theme"}>
+                                    <input type="checkbox" onChange={() => {
+                                        setTheme(!theme);
+                                        localStorage.setItem("theme", theme ? "light" : "dark");
+                                    }} checked={theme} id='theme' />
+                                </label>
+                            </button>
+                        </div>
                     </div>
                 </details>
-                <div></div>
             </div>
         </section>
     </header>
